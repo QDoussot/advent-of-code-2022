70b5f9b1c878e210730d792cc0b8ac9590d1ac91
Quentin Doussot <quentin.doussot@jellysmack.com>
2022-12-10 14:15:57 +0100

    Enable precise spliting (first/exact) for couple parser

--- src/parse/couple.rs
+++ src/parse/couple.rs
@@ -1,25 +1,51 @@
-use std::{collections::VecDeque, marker::PhantomData};
+use std::{collections::VecDeque, marker::PhantomData, str::from_utf8};
 
 use super::{separator::Separator, Context, Error, Parse, ParseExt};
 
+#[derive(PartialEq, Eq)]
+pub enum SplitMode {
+    Exact,
+    First,
+}
+
+pub trait SplitModeLike {
+    fn split_mode() -> SplitMode;
+}
+
+pub struct Exact {}
+impl SplitModeLike for Exact {
+    fn split_mode() -> SplitMode {
+        SplitMode::Exact
+    }
+}
+
+pub struct SplitFirst {}
+impl SplitModeLike for SplitFirst {
+    fn split_mode() -> SplitMode {
+        SplitMode::First
+    }
+}
+
 #[derive(Debug)]
-pub struct Couple<T1: Parse + Default, S: Separator, T2: Parse + Default> {
+pub struct Couple<T1: Parse + Default, S: Separator, T2: Parse + Default, SM: SplitModeLike = Exact> {
     previous_context: Context,
     p1: PhantomData<T1>,
     p2: PhantomData<T2>,
     sep: PhantomData<S>,
+    split_mode: PhantomData<SM>,
     res: (Option<T1::Out>, Option<T2::Out>),
     accepted: Vec<u8>,
     potential: VecDeque<u8>,
 }
 
-impl<T1: Parse + Default, T2: Parse + Default, S: Separator> Default for Couple<T1, S, T2> {
+impl<T1: Parse + Default, T2: Parse + Default, S: Separator, SM: SplitModeLike> Default for Couple<T1, S, T2, SM> {
     fn default() -> Self {
         Self {
             previous_context: Default::default(),
             p1: Default::default(),
             p2: Default::default(),
             sep: Default::default(),
+            split_mode: Default::default(),
             res: Default::default(),
             accepted: Default::default(),
             potential: Default::default(),
@@ -27,10 +53,15 @@ impl<T1: Parse + Default, T2: Parse + Default, S: Separator> Default for Couple<
     }
 }
 
-impl<T1: Parse + Default, T2: Parse + Default, S: Separator> Parse for Couple<T1, S, T2> {
+impl<T1: Parse + Default, T2: Parse + Default, S: Separator, SM: SplitModeLike> Parse for Couple<T1, S, T2, SM>
+where
+    T1::Out: std::fmt::Debug,
+    T2::Out: std::fmt::Debug,
+{
     type Out = (T1::Out, T2::Out);
 
     fn read_byte(&mut self, byte: &u8, context: Context) -> Result<(), Error> {
+        self.potential.push_back(*byte);
         if self.potential.len() == S::as_bytes().len() {
             if self.potential.make_contiguous() == S::as_bytes() {
                 if self.res.0.is_none() {
@@ -38,7 +69,7 @@ impl<T1: Parse + Default, T2: Parse + Default, S: Separator> Parse for Couple<T1
                     self.res.0 = Some(item);
                     self.potential.clear();
                     self.accepted.clear();
-                } else {
+                } else if SM::split_mode() == SplitMode::Exact {
                     return Err(Error::new("More than one field", "", context.line));
                 }
             } else if let Some(byte) = self.potential.pop_front() {
@@ -48,13 +79,12 @@ impl<T1: Parse + Default, T2: Parse + Default, S: Separator> Parse for Couple<T1
                 self.accepted.push(byte);
             }
         }
-        self.potential.push_back(*byte);
         Ok(())
     }
 
     fn end(mut self, context: Context) -> Result<Self::Out, Error> {
         if !self.potential.is_empty() {
-            if self.potential == S::as_bytes() {
+            if self.potential == S::as_bytes() && SM::split_mode() == SplitMode::Exact {
                 return Err(Error::new(
                     "",
                     format!("Final unexpected separator: '{:?}'", S::as_bytes()),
@@ -66,16 +96,14 @@ impl<T1: Parse + Default, T2: Parse + Default, S: Separator> Parse for Couple<T1
         }
         if self.res.0.is_none() {
             return Err(Error::new(
-                "",
+                from_utf8(&self.accepted).unwrap(),
                 format!("Reached end without finding separator: '{:?}'", S::as_bytes()),
-                0,
+                context.line,
             ));
         }
-        if !self.accepted.is_empty() {
-            match T2::parse_with_context(&self.accepted, context) {
-                Ok(item) => self.res.1 = Some(item),
-                Err(err) => return Err(err),
-            }
+        match T2::parse_with_context(&self.accepted, context) {
+            Ok(item) => self.res.1 = Some(item),
+            Err(err) => return Err(err),
         }
         Ok((self.res.0.unwrap(), self.res.1.unwrap()))
     }
@@ -92,5 +120,12 @@ mod tests {
         let couple = "200 Coucou".as_bytes();
         let couple = Couple::<Natural<usize>, SpaceSep, Natural<String>>::parse(couple).unwrap();
         assert_eq!((200, "Coucou".to_string()), couple);
+    }
+
+    #[test]
+    fn it_separates_at_first_separator_when_specified() {
+        let couple = "200 Coucou les loulous".as_bytes();
+        let couple = Couple::<Natural<usize>, SpaceSep, Natural<String>, SplitFirst>::parse(couple).unwrap();
+        assert_eq!((200, "Coucou les loulous".to_string()), couple);
     }
 }
